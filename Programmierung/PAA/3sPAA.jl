4d47c04824a0995598585e7ee96405b97b5a9c32
Elly home <elly@oh14.de>
2015-06-24 09:31:11 +0200

    0 am anfang gekuerzt, plotversuche, params

--- Programmierung/PAA/3sPAA.jl
+++ Programmierung/PAA/3sPAA.jl
@@ -1,10 +1,9 @@
-#using PyPlot
+using PyPlot
 
 
 function cut_Distributions(distributions, index)
     # Wenn Werte zu Beginn oder Schluss der Arrays zu klein sind, werden diese abgeschnitten
     # Dazu muessen alle Arrays zu kleine Werte an der entsprechenden Stelle haben
-    
 #     if (length(distributions[1]) < 200)
 #        return distributions, index
 #      end
@@ -102,6 +101,7 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
     #println(distributions)
     
     index = 1
+    zeitverschiebung = 0
     
     for i = 1:maxTime
         if  !(value > index)
@@ -110,17 +110,8 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
             #println (distributions)
             break
         end
-         
-#         if (sum(result)) > 1
-#             println ("break, summe erreicht")
-#             break
-#         end
-        
         distributions, index = updateDistributions!(params, distributions, index, num_states)
-    
         if (length(distributions[1]) > value-index)
-            #println(" d1 ", length(distributions[1]), " length ", (value-index+1) )
-            
             summe = 0
             for i in 1:num_states
                 summe += pop!(distributions[i])
@@ -128,19 +119,23 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
             push!(result, summe)
         else
             #das wird im prinzip nicht benoetigt, wenn man den index nutzt
-            push!(result, 0.0f0)
+            # TODO: Drüber nachdenken, das mit dem Index stimmt nämlich nicht
+            #push!(result, 0.0f0)
+            zeitverschiebung += 1
             #println("FAlse")
         end
-        #println(" result ", length(result))
         end
-    #println(result)
+    unshift!(result, zeitverschiebung)
+    println("index ", result[1])
     return result
 end
 
 function combineParams()
-    pmms = [0.005f0, 0.007f0, 0.01f0, 0.1f0, 0.3f0, 0.5f0, 0.7f0, 0.9f0, 0.99f0]
+    pmms = [0.005f0, 0.007f0, 0.01f0, 0.05f0, 0.1f0, 0.2f0, 0.3f0]
+    #pmms = [0.005f0, 0.007f0, 0.01f0, 0.1f0, 0.2f0, 0.3f0, 0.5f0, 0.7f0, 0.9f0, 0.99f0]
     # 0.00001f0 ist zu klein, nicht ausreichend teilchen drin, daher kein schönes tailing
-    pmls = [0.001f0, 0.0005f0, 0.0003f0, 0.0001f0, 0.0003f0, 0.00005f0, 0.00001f0] 
+    pmls = [0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0] 
+    #pmls = [0.01f0, 0.005f0, 0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0, 0.00003f0, 0.00005f0, 0.00001f0] 
     pms = Array(Any, 0)
     for pmm in pmms
         for pml in pmls
@@ -149,7 +144,8 @@ function combineParams()
         end 
     end
        
-    paas = [0.995f0, 0.997f0, 0.998f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0, 0.9999f0]   
+    paas = [0.999f0, 0.9991f0, 0.9992f0, 0.9993f0]   
+    #paas = [0.994f0, 0.995f0, 0.997f0, 0.998f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0, 0.9999f0]   
     pas = Array(Any, 0)
     for paa in paas
         pam = 1 - paa
@@ -158,7 +154,8 @@ function combineParams()
     #print (pas)
     
     #0.999999f0 ist zu groß
-    plls = [0.999925f0, 0.99995f0, 0.99975f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999997f0, 0.999999f0]
+    plls = [0.99995f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999996f0, 0.999997f0]
+    #plls = [0.9999f0, 0.999925f0, 0.99995f0, 0.999975f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999996f0, 0.999997f0, 0.999999f0]
     pls = Array(Any, 0)
     for pll in plls
         plm = 1 - pll
@@ -177,58 +174,63 @@ function combineParams()
 end
 
 
+function updateSims(directory)
+    println ("update")
+    
+end
+
 # for i = 1:5
 #     @time(waitingTimeForValue(0.9992f0, 0.99f0, 1000, 1000000))
 # end
 laenge = 1000
 maxtime = 2400000
 
-params = [0.4f0 0.599f0 0.001f0; 0.01f0 0.99f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
-params = [0.5f0 0.495f0 0.005f0; 0.0004f0 0.9996f0 0.0f0; 0.0001f0 0.0f0 0.9999f0]
-#params = [0.999f0 0.001f0 0.00f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+#params = [0.4f0 0.599f0 0.001f0; 0.01f0 0.99f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+#params = [0.8f0 0.195f0 0.005f0; 0.0004f0 0.9996f0 0.0f0; 0.0001f0 0.0f0 0.9999f0]
+params = [0.9f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
 
 param_list = combineParams()
 #reverse!(param_list)
 println(length(param_list))
 
-for params in param_list
+#for params in param_list
     println(strftime(time()))
-    #println (params)
-    if isfile("savedata_julia/l$laenge/$params")
-   #    println("Summe ", sum(res))
-   #    if (sum(res) < 0.999 )
-   #        println("zu gering bei $params")
-   #    end
-        filename = "savedata_julia/l$laenge/Sim"
-        #println (filename, " ")
-        for i in 1:3
-            for j in 1:3
-        #        println (i, " ", params[i, j])
-                filename = filename * "_" * string(params[i, j])
-                #println (filename)
-            end
-        end
-        if !isfile(filename)
-            println ("filename: ", filename, " ")
-            res = readcsv("savedata_julia/l$laenge/$params")
-            writecsv(filename, res)
-        end
-        #println ("isfile, $params")
-    #else
-        #for i in 1:10
-#         res = @time(waitingTimeForValue(params, laenge, maxtime))
-#         println("Summe ", sum(res))
-#         #end
-#         if (sum(res) >= 0.9 )
-#             plt.plot(res)
-#             plt.ylabel("")
-#      
-#      plt.xlabel("Zeit / Schritten")
-#             plt.title("PAA; Params: $params")
-#             plt.savefig("savefigs_julia/l$laenge/$params .png")
-#             plt.clf()
+    println (params)
+#     if isfile("savedata_julia/l$laenge/$params")
+#    #    println("Summe ", sum(res))
+#    #    if (sum(res) < 0.999 )
+#    #        println("zu gering bei $params")
+#    #    end
+#         filename = "savedata_julia/l$laenge/Sim"
+#         #println (filename, " ")
+#         for i in 1:3
+#             for j in 1:3
+#         #        println (i, " ", params[i, j])
+#                 filename = filename * "_" * string(params[i, j])
+#                 #println (filename)
+#             end
 #         end
-#         writecsv("savedata_julia/l$laenge/$params", res)
-   end
+#         if !isfile(filename)
+#             println ("filename: ", filename, " ")
+#             res = readcsv("savedata_julia/l$laenge/$params")
+#             writecsv(filename, res)
+#         end
+#         #println ("isfile, $params")
+#     else
+        #for i in 1:10
+        res = @time(waitingTimeForValue(params, laenge, maxtime))
+        println("Summe ", sum(res))
+#         #end
+        writecsv("savedata_julia/l$laenge/$params", res)
+        if (sum(res) >= 0.9 )
+            plt.plot(res[2:end])
+            #plt.xticks([0, len(res)], [res[1], (len(res) + res[0]))
+            plt.ylabel("")
+            plt.xlabel("Zeit / Schritten")
+            plt.title("PAA; Params: $params")
+            plt.savefig("savefigs_julia/l$laenge/$params .png")
+            plt.clf()
+#        end
+#    end
 end  
 # println("fertig")
