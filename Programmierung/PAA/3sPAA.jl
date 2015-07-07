39b06a1cdd4a4fd865f2be78a02f22468a45e3de
Elly home <elly@oh14.de>
2015-07-07 13:00:51 +0200

    kleine parameteraenderungen

--- Programmierung/PAA/3sPAA.jl
+++ Programmierung/PAA/3sPAA.jl
@@ -1,43 +1,43 @@
-using PyPlot
+#using PyPlot
 
 
 function cut_Distributions(distributions, index)
     # Wenn Werte zu Beginn oder Schluss der Arrays zu klein sind, werden diese abgeschnitten
     # Dazu muessen alle Arrays zu kleine Werte an der entsprechenden Stelle haben
-#     if (length(distributions[1]) < 200)
-#        return distributions, index
-#      end
+    
     vorneschneiden = true
-    # Vorne testen
+    # Vorne testen, falls ein Wert gross genug ist, wird nicht geschnitten
     for i in 1:length(distributions)
         if distributions[i][1] >= 1.0f-17
             vorneschneiden = false
             break
         end
     end
-    #Vorne abschneiden
+    #Vorne abschneiden durch shift! aller Arrays, gleichzeitig Index verschieben
     if vorneschneiden
         index += 1
-        #println ("shift", (distributions[1]))
         for i in 1:length(distributions)
-        #    print (" ", distributions[i][1])
+            #distributions[i][1] = 0.0f0
             shift!(distributions[i])
-        #    println (" ", distributions[i][1])
         end
+        #print (" shiftindex, indexneu: $index, laenge dist ")
+        #println (length(distributions[1]))
     end
+    
     hintenschneiden = true
-    # Hinten testen    
+    # Hinten testen, wie vorne    
     for i in 1:length(distributions)
         if distributions[i][end] >= 1.0f-17
             hintenschneiden = false
             break
         end
     end
-    # Hinten abschneiden
+    # Hinten abschneiden durch pop! aller Arrays
     if hintenschneiden
         for i in 1:length(distributions)
             pop!(distributions[i])
         end
+        #print ("hintenschneiden")
     end
     return distributions, index
 end
@@ -50,25 +50,18 @@ function updateDistributions!(params::Array{Float32,2}, distributions, index, nu
     =#
     # Erstelle Hilfsmatrix aus Arrays. Jedes Array wird später befüllt aus einer alten Verteilung mal entsprechender Uebergangswahrscheinlichkeit. Bei 3 Zustaenden ergeben sich so 9 Arrays
     hilfsdings = Array(Any, num_states)
-    #println ("hilfsdings beim befuellen ", hilfsdings, typeof(hilfsdings))
     for i in 1:num_states
         hilfsdings[i] = Array(Any, num_states)
     end
     
-    # Inhalte der Hilfsarrays berechnen
+    # Inhalte der Hilfsarrays berechnen, Erklaerung siehe Zettel vom 10.6.15.
     for i in 1:num_states
         for j in 1:num_states
-            #print ("dist $i ", distributions[i])
-            #print (" params $i $j ", params[i, j])
-            #println (" multipiziert ", distributions[i] * params[i, j])
-            #println("hilfsdings i ", hilfsdings[1])
             hilfsdings[i][j] = (distributions[i] * params[i, j])
-            #println ("hilfsdingsij ", hilfsdings[i][j])
         end
     end
-    #println("hilfsdings ", hilfsdings)
     
-    # Verschieben der mobilen Daten. Als Ausgleich bei den anderen Arrays ein Feld hinten anfuegen
+    # Verschieben der mobilen Daten. Als Ausgleich bei den anderen Arrays ein Feld hinten anfuegen, da die Arrays gleiche Laenge brauchen
     for i in 1:num_states
         unshift!(hilfsdings[1][i], 0.0f0)
         for j in 2:num_states
@@ -85,6 +78,7 @@ function updateDistributions!(params::Array{Float32,2}, distributions, index, nu
             new_dist[i] += hilfsdings[j][i]
         end
     end
+    # Zu kleine Werte wegwerfen
     new_dist, index = cut_Distributions(new_dist, index)
 
     return new_dist, index        
@@ -93,48 +87,56 @@ end
 
 function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_states = 3)
     # Beginne mit quasi leerem ergebnis, wkeitsmasse 1 in mobil und 0 in stat
-    #println ("params ", params)
     result = zeros(Float32, 1)
-
+    #println("Starte Sim")
     #TODO fuer andere anzahl an zustaenden
     distributions = Array[[1],[0],[0]]
-    #println(distributions)
     
+    #Index noetig, wenn zu kleine W'keiten am Arraybeginn rausgeschmissen werden
     index = 1
+    # Zeitverschiebung als Offset fuer den Peakbeginn
     zeitverschiebung = 0
     
+    # Simuliere jeden Schritt bis maxTime
     for i = 1:maxTime
+        # Ziel vorzeitig erreicht
         if  !(value > index)
-            #Ziel erreicht
             println ("break, fertig nach $i Schritten")
-            #println (distributions)
             break
-        end
+        end 
+        # Ein Berechungsschritt
         distributions, index = updateDistributions!(params, distributions, index, num_states)
+        # Muss jeden Schritt notieren wie viel % der Teilchen ankommen, entweder als Summe ueber die angekommenen Teilchen (if)
+        # oder wenn keine angekommen sind (else) 0 bzw offset erhoehen
         if (length(distributions[1]) > value-index)
             summe = 0
             for i in 1:num_states
                 summe += pop!(distributions[i])
             end
+            #println("angekommen, $summe")
             push!(result, summe)
         else
-            #das wird im prinzip nicht benoetigt, wenn man den index nutzt
-            # TODO: Drüber nachdenken, das mit dem Index stimmt nämlich nicht
             #push!(result, 0.0f0)
             zeitverschiebung += 1
-            #println("FAlse")
         end
-        end
+    end
+    #TODO, das geht sicher eleganter. Momentan wird der Offset einfach als ersten Wert ins Result geschrieben
+    # ist einfach, erfordert Aufpassen bei der Nachverarbeitung
     unshift!(result, zeitverschiebung)
-    println("index ", result[1])
     return result
 end
 
 function combineParams()
-    pmms = [0.005f0, 0.007f0, 0.01f0, 0.05f0, 0.1f0, 0.2f0, 0.3f0]
-    #pmms = [0.005f0, 0.007f0, 0.01f0, 0.1f0, 0.2f0, 0.3f0, 0.5f0, 0.7f0, 0.9f0, 0.99f0]
+    # Benennung der Wahrscheinlichkeiten:
+    # p plus Startzustand, Endzustand, s bei Liste
+    # Moegliche Zustaende: m - mobil, a - Adsorbtion, l - Loesung
+    # TODO: Aktuell nur Kombination fuer Modell 3a)
+    
+    #Erstelle zunaechst Listen fuer Einzelwahrscheinlichkeiten fuer mobilen Ausgangszustand...
+    pmms = [0.05f0, 0.1f0, 0.2f0, 0.3f0, 0.4f0, 0.5f0, 0.6f0, 0.7f0, 0.8f0]
+    #pmms = [0.005f0, 0.007f0, 0.01f0, 0.03f0, 0.05f0, 0.1f0, 0.2f0, 0.3f0, 0.4f0, 0.5f0, 0.6f0, 0.7f0, 0.8f0, 0.9f0, 0.99f0]
     # 0.00001f0 ist zu klein, nicht ausreichend teilchen drin, daher kein schönes tailing
-    pmls = [0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0] 
+    pmls = [0.005f0, 0.001f0, 0.0007f0, 0.0005f0, 0.0003f0] 
     #pmls = [0.01f0, 0.005f0, 0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0, 0.00003f0, 0.00005f0, 0.00001f0] 
     pms = Array(Any, 0)
     for pmm in pmms
@@ -143,28 +145,24 @@ function combineParams()
             push!(pms,([pmm pma pml]))
         end 
     end
-       
-    paas = [0.999f0, 0.9991f0, 0.9992f0, 0.9993f0]   
-    #paas = [0.994f0, 0.995f0, 0.997f0, 0.998f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0, 0.9999f0]   
+    # ... fuer adsorbierten Ausgangszustand...  
+    paas = [0.998f0, 0.9985f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0]   
+    #paas = [0.994f0, 0.995f0, 0.997f0, 0.998f0, 0.9985f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0, 0.9999f0]   
     pas = Array(Any, 0)
     for paa in paas
         pam = 1 - paa
         push!(pas,([pam paa 0.0f0]))
     end
-    #print (pas)
-    
+    # ... und fuer geloesten Ausgangszustand
+    plls = [0.9999f0, 0.999925f0,0.99995f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999999f0]
     #0.999999f0 ist zu groß
-    plls = [0.99995f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999996f0, 0.999997f0]
     #plls = [0.9999f0, 0.999925f0, 0.99995f0, 0.999975f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999996f0, 0.999997f0, 0.999999f0]
     pls = Array(Any, 0)
     for pll in plls
         plm = 1 - pll
         push!(pls,([plm 0.0f0 pll]))
     end
-    
-  #  println("pms", pms)
-  #  println("pas", pas)
-  #  println("pls", pls)
+    # erstelle Liste aller moeglichen Kombinationen
     param_list = Array(Any, 0)
     for pm in pms, pa in pas, pl in pls
                 push!(param_list,([pm, pa, pl])) 
@@ -187,50 +185,54 @@ maxtime = 2400000
 
 #params = [0.4f0 0.599f0 0.001f0; 0.01f0 0.99f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
 #params = [0.8f0 0.195f0 0.005f0; 0.0004f0 0.9996f0 0.0f0; 0.0001f0 0.0f0 0.9999f0]
-params = [0.9f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+#params = [0.9f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+#params[0.5 0.49995 5.0e-5; 0.004999995 0.995 0.0; 1.001358e-5 0.0 0.99999] 
 
 param_list = combineParams()
 #reverse!(param_list)
 println(length(param_list))
 
-#for params in param_list
-    println(strftime(time()))
-    println (params)
-#     if isfile("savedata_julia/l$laenge/$params")
-#    #    println("Summe ", sum(res))
-#    #    if (sum(res) < 0.999 )
-#    #        println("zu gering bei $params")
-#    #    end
-#         filename = "savedata_julia/l$laenge/Sim"
+for params in param_list
+    filename = "savedata_julia/l$laenge/Sim"
+    for i in 1:3
+        for j in 1:3
+    #        println (i, " ", params[i, j])
+            filename = filename * "_" * string(params[i, j])
+            #println (filename)
+        end
+    end
+    if !isfile(filename)
+   # if isfile("savedata_julia/l$laenge/$params")
+   #    println("Summe ", sum(res))
+   #    if (sum(res) < 0.999 )
+   #        println("zu gering bei $params")
+   #    end
 #         #println (filename, " ")
-#         for i in 1:3
-#             for j in 1:3
-#         #        println (i, " ", params[i, j])
-#                 filename = filename * "_" * string(params[i, j])
-#                 #println (filename)
-#             end
-#         end
 #         if !isfile(filename)
 #             println ("filename: ", filename, " ")
 #             res = readcsv("savedata_julia/l$laenge/$params")
 #             writecsv(filename, res)
 #         end
-#         #println ("isfile, $params")
-#     else
-        #for i in 1:10
+#        println ("isfile, $params")
+    #else
+#         #for i in 1:10
+        println (params)
+        println(strftime(time()))
         res = @time(waitingTimeForValue(params, laenge, maxtime))
-        println("Summe ", sum(res))
+        println ("offset ", res[1])
+        println("Summe ", sum(res[2:end]))
 #         #end
-        writecsv("savedata_julia/l$laenge/$params", res)
-        if (sum(res) >= 0.9 )
-            plt.plot(res[2:end])
-            #plt.xticks([0, len(res)], [res[1], (len(res) + res[0]))
-            plt.ylabel("")
-            plt.xlabel("Zeit / Schritten")
-            plt.title("PAA; Params: $params")
-            plt.savefig("savefigs_julia/l$laenge/$params .png")
-            plt.clf()
-#        end
-#    end
+        writecsv(filename, res)
+    end
+#         if (sum(res) >= 0.9 )
+#             plt.plot(res[2:end])
+#             #plt.xticks([0, len(res)], [res[1], (len(res) + res[0]))
+#             plt.ylabel("")
+#             plt.xlabel("Zeit / Schritten")
+#             plt.title("PAA; Params: $params")
+#             plt.savefig("savefigs_julia/l$laenge/$params .png")
+#             plt.clf()
+       # end
+    end
 end  
 # println("fertig")
--- Programmierung/PAA/sim_PAA.py
+++ Programmierung/PAA/sim_PAA.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*- 
+# -*- coding: utf-8 -*- 
 # Weiterverarbeitung einer PAA-Simulation aus Julia
 #TODO: mosdi-kompatibel machen durch abfrage nach erstem wert der wahrscheinlichkeitsliste
 
@@ -17,37 +17,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class PAA():
-    def __init__(self, params, length, times, maxtime = 240, source="julia"):
+    version_number = 1.0
+    
+    def __init__(self, params, length, times, maxtime = 240, source="julia", comp_factor = 1000, version = version_number):
         self.params = params
         self.length = length
         self.maxtime = maxtime
         self.times = times
         self.source = source
-        self.compressed, self.offset = self.compress()
+        # nur bei Julia:
+        if source == "julia":
+            self.offset = times.pop(0)/10000
+        # Bei mosdi sind immer die 0 am Anfang, offset gibt es da nicht
+        else:
+            self.offset = 0
         self.pd = self.calculate_pd()
+        self.times = self.compress(comp_factor)
+        self.version = version
     
     def compress(self, comp_factor = 1000):
         '''Fasst Datenpunkte zusammen'''
-        offset = self.times.pop(0)/comp_factor 
         comp_probs = []
         for i in range(comp_factor):
             secsum = 0
             for j in range((len(self.times)//comp_factor)):
                 secsum += self.times[i*(len(self.times)//comp_factor) + j]
             comp_probs.append(secsum)  
-        # Bei mosdi sind immer die 0 am Anfang, offset gibt es da nicht
-        if self.source != "julia":
-            return comp_probs, 0
-        return comp_probs, offset
+        return comp_probs
     
     def calculate_pd(self):
         '''Berechne Peakdaten: Maximalzeitpunkt, Quartile, IQR und IQK'''
         #times = np.array(self.times)
-        times = np.array(self.compressed)
+        times = np.array(self.times)
         # wsumme ist Summe aller W'keiten, soll 1 sein, ist aber auf Grund von Rundungsungenauigkeiten oft nicht so
         wsumme = sum(times)
         if wsumme < 0.99:
-            print ("Summe der Wahrscheinlichkeiten zu gering, moeglicherweise ragt Peak ueber die Maxtime %s hinaus", self.maxtime)
+            print ("Summe der Wahrscheinlichkeiten zu gering, moeglicherweise ragt Peak ueber die Maxtime ", self.maxtime, " hinaus")
             iqr, qk = float("nan"), float("nan")
             return ([self.offset + np.argmax(times), [float("nan"), float("nan"), float("nan")], float("nan"), float("nan")]) 
         print ("summe", wsumme)
@@ -59,45 +64,108 @@ class PAA():
         while p25 < 0.25*wsumme:
             p25+= times[i]
             i += 1
-        quartiles[0] = i
+        quartiles[0] = i/10000
         p50 = p25
         while p50 < 0.5 * wsumme:
             p50 += times[i]
             i += 1
-        quartiles[1] = i
+        quartiles[1] = i/10000
         p75 = p50
         while p75 < 0.75 * wsumme:
             p75 += times[i]
             i += 1
-        quartiles[2] = i
-        print ("quartile", quartiles)
+        quartiles[2] = i/10000
+        #print ("quartile", quartiles)
         # Interquartilsabstand (Breite)
         iqr = (quartiles[2] - quartiles[0])
         # Interquantilskoeffizient (Schiefe)
         qk = (quartiles[2] + quartiles[0] - 2*quartiles[1]) / (quartiles[2] - quartiles[0])
         # Lage des Maximums, quartile, iqr, qk
-        peakdata = ([self.offset + np.argmax(times), quartiles, iqr, qk]) 
+        peakdata = ([(self.offset + np.argmax(times))/10000, quartiles, iqr, qk]) 
         #plt.plot(times)
         #plt.show()
+        print (peakdata)
         return peakdata
+    
+    
+def erzeuge_Tabelle(directory, csv_name, time_range = [1,240], iqr_range = [0,100], iqk_range = [0,1]):
+    filenames = [name for name in os.listdir(directory) if name.startswith("Sim_")]
+    starttime = time.clock()
+    peaks_found = []
+    for filename in filenames:
+        params = [float(p) for p in filename[4:].strip(".p").split("_")]
+        #print (filename)
+        #print (directory + filename)
+        with open(directory+filename, "rb") as mydata:
+         #   print (mydata)
+        #    myPAA = PAA.PAA(params, 1000, [0, 0.5, 0.5])
+            myPAA = pickle.load(mydata)
+            #print (myPAA)
+            #time.sleep(1)
+            if myPAA.pd[0] > time_range[0] and myPAA.pd[0] < time_range[1] and myPAA.pd[2] > iqr_range[0] and myPAA.pd[2] < iqr_range[1] and myPAA.pd[3] > iqk_range[0] and myPAA.pd[3] < iqk_range[1]:
+                #print ("  passt  ")
+                #plt.plot(mytimes)
+                params.extend(myPAA.pd) 
+                peaks_found.append(params)
+                #plt.show()
+    print ("zeit", time.clock() - starttime)       
+    #TODO aus den Daten die PD, also Loc, IQR und QK 
+    with open(csv_name, 'w', newline='') as csvfile:
+        mywriter = csv.writer(csvfile)
+        #mywriter.writerows (tabelle)
+        print ("laengen", len(peaks_found))
+        for i in range(len(peaks_found)):
+            mywriter.writerow(peaks_found[i])
+    print ("fertig mit Tabelle")  
+    return peaks_found
+
+
+def plotte_Zeitpunkt(directory, time_range = [1,240], iqr_range = [0,100], iqk_range = [0,1]):
+    filenames = [name for name in os.listdir(directory) if name.startswith("Sim_")]
+    tabellenname = "xy_" + str(time_range[0]) + "_" + str(time_range[1]) + "_" + str(iqr_range[0]) + "_" + str(iqr_range[1]) + "_"+  str(iqk_range[0]) + "_" + str(iqk_range[1]) + ".csv"
+    peaks_found = erzeuge_Tabelle(directory, tabellenname, time_range, iqr_range, iqk_range)
+    starttime = time.clock()
+    fig = plt.figure()
+    ax0 = fig.add_subplot(221)
+    ax0.set_title("pmm")
+    ax1 = fig.add_subplot(222)
+    ax1.set_title("pml")
+    ax2 = fig.add_subplot(223)
+    ax2.set_title("paa")
+    ax3 = fig.add_subplot(224)
+    ax3.set_title("pll")
+    ax = [ax0, ax1, ax2, ax3]
+    for peak in peaks_found:
+        print (peak)
+        #TODO: Die markersize sinnvoll nutzen
+        for i, j in enumerate([0, 2, 4, 8]):
+            ax[i].plot([peak[11]], [peak[12]], "go", markersize = (time_range[1]-peak[9]))
+            t = ax[i].text(peak[11], peak[12], str(peak[j]), size= "small")
+        #for p in peak:
+            #print (p)
+            #time.sleep(1)
+    plt.show()
+        
     
 def main():
     source_directory = "savedata_julia/l1000/"
-    
     dest_directory = "savedata_python/l1000/"
+    fig_dir = "savefigs_python/l1000"
     filenames = [name for name in os.listdir(source_directory) if name.startswith("Sim_")]
+    #filenames.reverse()
+    plotte_Zeitpunkt(dest_directory, [30,70], [1,15], [0.2,1])  
     for filename in filenames:
-        print(filename)
         if not os.path.exists(dest_directory+filename+".p"):
+            print(filename)
             with open (source_directory + filename , "r" ) as data:
-                times = [float(x) for x in data]
-                params = [float(p) for p in filename[4:].split("_")]
-                aPAA = PAA(params, 1000, times)
-                with open(dest_directory+filename +".p", "wb") as savedata:
-                    pickle.dump(aPAA, savedata)
-                    print("...")
+                    times = [float(x) for x in data]
+                    params = [float(p) for p in filename[4:].split("_")]
+                    aPAA = PAA(params, 1000, times)
+                    with open(dest_directory+filename +".p", "wb") as savedata:
+                        pickle.dump(aPAA, savedata)
+    #erzeuge_Tabelle(dest_directory, "mycsv.csv", [40,60], [1,20])
         
        
 if __name__ == "__main__":
     logging.basicConfig(level=20)
-    main() 
+    main()      
