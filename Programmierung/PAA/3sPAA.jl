7b22d38d4a1a2cb343f12f7f03ed33f56ca08d29
Elly home <elly@oh14.de>
2015-07-22 08:58:53 +0200

    minimal

--- Programmierung/PAA/3sPAA.jl
+++ Programmierung/PAA/3sPAA.jl
@@ -1,10 +1,10 @@
-using PyPlot
-
+#using PyPlot
+# Implementierung eines PAAs fuer 3 Zustaende, sollte mit minimalen Aenderungen (zB init der dist) auf fuer mehr Zustaende funktionieren
+# Nur der benoetigte Spezialfall eines PAA
 
 function cut_Distributions(distributions, index)
     # Wenn Werte zu Beginn oder Schluss der Arrays zu klein sind, werden diese abgeschnitten
-    # Dazu muessen alle Arrays zu kleine Werte an der entsprechenden Stelle haben
-    
+    # Dazu muessen alle Arrays zu kleine Werte an der entsprechenden Stelle haben  
     vorneschneiden = true
     # Vorne testen, falls ein Wert gross genug ist, wird nicht geschnitten
     for i in 1:length(distributions)
@@ -20,8 +20,6 @@ function cut_Distributions(distributions, index)
             #distributions[i][1] = 0.0f0
             shift!(distributions[i])
         end
-        #print (" shiftindex, indexneu: $index, laenge dist ")
-        #println (length(distributions[1]))
     end
     
     hintenschneiden = true
@@ -37,7 +35,6 @@ function cut_Distributions(distributions, index)
         for i in 1:length(distributions)
             pop!(distributions[i])
         end
-        #print ("hintenschneiden")
     end
     return distributions, index
 end
@@ -45,7 +42,7 @@ end
 function updateDistributions!(params::Array{Float32,2}, distributions, index, num_states)
     #= ps, pm: Parameter
        distributions: Matrix die Werte fuer mobil/stat enthaelt (Wert: WKeit an jeweiliger position)
-       index: zur spaeteren Verschiebung des Arrays
+       index: zur spaeteren Verschiebung des Arrays, wird erhoeht, wenn zu kleine Werte abgeschnitten werden
        return: Neue distributions, index
     =#
     # Erstelle Hilfsmatrix aus Arrays. Jedes Array wird später befüllt aus einer alten Verteilung mal entsprechender Uebergangswahrscheinlichkeit. Bei 3 Zustaenden ergeben sich so 9 Arrays
@@ -78,9 +75,8 @@ function updateDistributions!(params::Array{Float32,2}, distributions, index, nu
             new_dist[i] += hilfsdings[j][i]
         end
     end
-    # Zu kleine Werte wegwerfen
+    # Zu kleine Werte wegwerfen, um Groesse der Arrays zu beschraenken
     new_dist, index = cut_Distributions(new_dist, index)
-
     return new_dist, index        
 end
 
@@ -94,8 +90,6 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
     
     #Index noetig, wenn zu kleine W'keiten am Arraybeginn rausgeschmissen werden
     index = 1
-    # Zeitverschiebung als Offset fuer den Peakbeginn
-    zeitverschiebung = 0
     
     # Simuliere jeden Schritt bis maxTime
     for i = 1:maxTime
@@ -107,7 +101,7 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
         # Ein Berechungsschritt
         distributions, index = updateDistributions!(params, distributions, index, num_states)
         # Muss jeden Schritt notieren wie viel % der Teilchen ankommen, entweder als Summe ueber die angekommenen Teilchen (if)
-        # oder wenn keine angekommen sind (else) 0 bzw offset erhoehen
+        # oder wenn keine angekommen sind (else) 0 reinschieben
         if (length(distributions[1]) > value-index)
             summe = 0
             for i in 1:num_states
@@ -116,26 +110,25 @@ function waitingTimeForValue(params::Array{Float32, 2}, value, maxTime, num_stat
             #println("angekommen, $summe")
             push!(result, summe)
         else
-            #push!(result, 0.0f0)
-            zeitverschiebung += 1
+            push!(result, 0.0f0)
         end
     end
-    #TODO, das geht sicher eleganter. Momentan wird der Offset einfach als ersten Wert ins Result geschrieben
-    # ist einfach, erfordert Aufpassen bei der Nachverarbeitung
-    unshift!(result, zeitverschiebung)
     return result
 end
 
+
 function combineParams()
-    # Benennung der Wahrscheinlichkeiten:
-    # p plus Startzustand, Endzustand, s bei Liste
+    # Verschiedene Parameterkombinationen erstellen
+    # Benennung der Wahrscheinlichkeiten: p plus Startzustand, Endzustand, s bei Liste
     # Moegliche Zustaende: m - mobil, a - Adsorbtion, l - Loesung
     # TODO: Aktuell nur Kombination fuer Modell 3a)
     
     #Erstelle zunaechst Listen fuer Einzelwahrscheinlichkeiten fuer mobilen Ausgangszustand...
-    pmms = [0.005f0, 0.01f0, 0.05f0, 0.1f0, 0.15f0, 0.2f0, 0.25f0, 0.3f0, 0.35f0, 0.4f0, 0.45f0, 0.5f0, 0.55f0, 0.6f0, 0.65f0, 0.7f0, 0.75f0, 0.8f0, 0.85f0, 0.9f0, 0.95f0]
+    #pmms = [0.1f0, 0.2f0, 0.3f0, 0.4f0, 0.5f0, 0.6f0, 0.7f0, 0.8f0, 0.9f0]
+    pmms = [0.005f0, 0.01f0, 0.05f0, 0.1f0, 0.15f0, 0.2f0, 0.25f0, 0.3f0, 0.35f0, 0.4f0, 0.45f0, 0.5f0, 0.55f0, 0.6f0, 0.65f0, 0.7f0, 0.75f0, 0.8f0, 0.85f0, 0.9f0]
     #pmms = [0.005f0, 0.007f0, 0.01f0, 0.03f0, 0.05f0,  0.1f0, 0.15f0, 0.2f0, 0.25f0, 0.3f0, 0.35f0, 0.4f0, 0.45f0, 0.5f0, 0.55f0, 0.6f0, 0.65f0, 0.7f0, 0.75f0, 0.8f0, 0.85f0, 0.9f0, 0.95f0, 0.99f0]
     # 0.00001f0 ist zu klein, nicht ausreichend teilchen drin, daher kein schönes tailing
+    #pmls = [0.005f0, 0.001f0, 0.0005f0, 0.0001f0] 
     pmls = [0.005f0, 0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0, 0.00005f0] 
     #pmls = [0.01f0, 0.005f0, 0.001f0, 0.0007f0, 0.0005f0, 0.0003f0, 0.0001f0, 0.00005f0, 0.00003f0, 0.00001f0] 
     pms = Array(Any, 0)
@@ -146,7 +139,8 @@ function combineParams()
         end 
     end
     # ... fuer adsorbierten Ausgangszustand...  
-    paas = [0.998f0, 0.9985f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0]   
+    #paas = [0.998f0, 0.9985f0, 0.999f0, 0.9992f0, 0.9994f0, 0.9996f0]   
+    paas = [0.997f0, 0.998f0, 0.9985f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0]   
     #paas = [0.994f0, 0.995f0, 0.997f0, 0.998f0, 0.9985f0, 0.999f0, 0.9991f0, 0.9992f0, 0.9993f0, 0.9994f0, 0.9995f0, 0.9996f0, 0.9999f0]   
     pas = Array(Any, 0)
     for paa in paas
@@ -154,6 +148,7 @@ function combineParams()
         push!(pas,([pam paa 0.0f0]))
     end
     # ... und fuer geloesten Ausgangszustand
+    #plls = [0.9999f0, 0.99995f0,  0.99999f0,  0.999995f0, 0.999999f0]
     plls = [0.9999f0, 0.999925f0, 0.99995f0, 0.999975f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999997f0, 0.999999f0]
     #0.999999f0 ist zu groß
     #plls = [0.9999f0, 0.999925f0, 0.99995f0, 0.999975f0, 0.99999f0, 0.999993f0, 0.999995f0, 0.999996f0, 0.999997f0, 0.999999f0]
@@ -187,20 +182,25 @@ maxtime = 2400000
 #params = [0.8f0 0.195f0 0.005f0; 0.0004f0 0.9996f0 0.0f0; 0.0001f0 0.0f0 0.9999f0]
 #params = [0.9f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
 #params[0.5 0.49995 5.0e-5; 0.004999995 0.995 0.0; 1.001358e-5 0.0 0.99999] 
-params = [0.5f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
-params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 5.0f-05 0.0f0 0.999995f0]
-param_list = Array(Any, 0)
-push!(param_list, params)
-params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
-push!(param_list, params)
-params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.00002f0 0.0f0 0.99998f0]
-push!(param_list, params)
-params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.000015f0 0.0f0 0.999985f0]
-push!(param_list, params)
-#param_list = combineParams()
-#reverse!(param_list)
+# params = [0.5f0 0.1f0 0.0f0; 0.001f0 0.999f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+# params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 5.0f-05 0.0f0 0.999995f0]
+ param_list = Array(Any, 0)
+# push!(param_list, params)
+# params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.00005f0 0.0f0 0.99995f0]
+# push!(param_list, params)
+# params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.000015f0 0.0f0 0.999985f0]
+# push!(param_list, params)
+# params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.0001f0 0.0f0 0.9999f0]
+# push!(param_list, params)
+#params = [0.4f0 0.59995f0 5.0f-05; 0.002f0 0.998f0 0.0f0; 0.000025f0 0.0f0 0.999975f0]
+#push!(param_list, params)
+
+# Parameterliste erstellen
+param_list = combineParams()
+reverse!(param_list)
 println(length(param_list))
 
+# Simulationen starten, vorher testen, ob diese schon exisitert
 for params in param_list
     filename = "savedata_julia/l$laenge/Sim"
     for i in 1:3
@@ -223,25 +223,23 @@ for params in param_list
 # #             writecsv(filename, res)
 # #         end
 # #        println ("isfile, $params")
-#     #else
-# #         #for i in 1:10
-        println (params)
-        println(strftime(time()))
+# #     else
+        println(strftime(time()), " starte: ")
+        print (params, " ")
         res = @time(waitingTimeForValue(params, laenge, maxtime))
         println ("offset ", res[1])
         println("Summe ", sum(res[2:end]))
-#         #end
         writecsv(filename, res)
    # end
-        if (sum(res) >= 0.9 )
-            plt.plot(res[2:end])
-            #plt.xticks([0, len(res)], [res[1], (len(res) + res[0]))
-            plt.ylabel("")
-            plt.xlabel("Zeit / Schritten")
-            plt.title("PAA; Params: $params")
-            plt.savefig("savefigs_julia/l$laenge/$params .png")
-            plt.clf()
-       end
+#         if (sum(res) >= 0.9 )
+#             plt.plot(res[2:end])
+#             #plt.xticks([0, len(res)], [res[1], (len(res) + res[0]))
+#             plt.ylabel("")
+#             plt.xlabel("Zeit / Schritten")
+#             plt.title("PAA; Params: $params")
+#             plt.savefig("savefigs_julia/l$laenge/$params .png")
+#             plt.clf()
+#        end
     end
 end  
 println("fertig")
